9cbec484876771f18464bd3127a019db419993d0
omar <omarliberatto1967@gmail.com>
2024-05-29 11:59:35 -0300

    carrier to carrierservices

--- database/migrations/2024_05_09_193824_create_carrier_services_table.php
+++ database/migrations/2024_05_09_193824_create_carrier_services_table.php
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('carriers');
+        Schema::dropIfExists('carrier_services');
     }
 };
